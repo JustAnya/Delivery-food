69366879dc993418e9d0425baa95470b7dc045e2
JustAnya <anna_s.mozol@mail.ru>
2020-12-24 23:35:08 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
 const cartButton = document.querySelector("#cart-button");
-const modal = document.querySelector(".modal");//обращаемся к элементу который имеет класс модал с помощью документ
+const modal = document.querySelector(".modal");
 const close = document.querySelector(".close");
 
 
 cartButton.addEventListener('click',function(event){
-    // console.log("YOu click on the button!")
-    // для модального окна повесим класс is open
- modal.classList.add("is-open");//мне нужно у этого модального окна получить список классов и добавить туда свой класс исопен
+ modal.classList.add("is-open");
 });
 
 close.addEventListener('click',function(event){
- modal.classList.remove("is-open");//мне нужно у этого модального окна получить список классов и удалить туда свой класс исопен
+ modal.classList.remove("is-open");
 });
 
 /////////second varian
